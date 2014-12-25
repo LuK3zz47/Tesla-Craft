f962853f7b11c4652aa482b93dccee9f8a83003a
JBuk <jbuk@jbuk.eu>
2014-12-25 16:14:44 +0100

    Building with Gradle
    Just run: gradlew build

--- src/java/eu/teslacraft/References.java
+++ src/java/eu/teslacraft/References.java
@@ -7,6 +7,6 @@ public class References {
 
     public static final String MOD_NAME = "TeslaCraft";
     public static final String MOD_ID = "TeslaCraft";
-    public static final String MOD_VERSION = "0.0.1";
+    public static final String MOD_VERSION = "@VERSION@";
 
 }
